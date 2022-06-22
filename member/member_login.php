7025106de069d3ee0bfe8f97c45993243e5d4a13
valentinoOKT <valentino.oktawan@student.umn.ac.id>
2022-06-22 16:09:53 +0700

    fixing bug

--- member/member_login.php
+++ member/member_login.php
@@ -7,7 +7,7 @@
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h4 class="modal-title">Masuk sebagai member</h4>
       </div>
-        <form class="form-horizontal" action="index1.php" method="post">
+        <form class="form-horizontal" action="../index1.php" method="post">
           <div class="form-group">
             <label class="control-label col-sm-3" for="username">Username</label>
             <div class="col-sm-8">
@@ -30,7 +30,6 @@
       </div>
     </div>
     <?php
-	include ("../index1.php.php");
 	if(isset($_POST['loginmember'])){
 		$username = $_POST['username'];
 		$password = $_POST['password'];
